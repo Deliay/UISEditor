c71ec5403066aa2798bf0fc6f02e96e69af9149a
Deliay <admin@remiliascarlet.com>
2017-12-22 17:36:13 +0800

    Render manager

---
+++ Render/RenderManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using UISEditor.Data;
+using UISEditor.View;
+
+namespace UISEditor.Render
+{
+    public class RenderManager
+    {
+        public static Canvas RenderLayer;
+
+        public static void Render()
+        {
+            foreach (var item in UISObjectTree.Instance)
+            {
+
+            }
+        }
+
+        public static void Render<T>(UISList<T> list) where T : UISObject
+        {
+
+        }
+    }
+
+}
