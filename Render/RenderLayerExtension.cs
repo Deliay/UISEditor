1a0dc18ee544895788cae3d7d9f66bb1700d4435
Deliay <admin@remiliascarlet.com>
2018-01-11 10:20:50 +0800

    RenderLayers' Extension method

---
+++ Render/RenderLayerExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UISEditor.Render
+{
+    public static class RenderLayerExtension
+    {
+        public static void AddTransform(this Grid view, Transform transform)
+        {
+            if (view.RenderTransform is TransformGroup tg) { }
+            else
+            {
+                tg = new TransformGroup();
+                view.RenderTransform = tg;
+            }
+
+            tg.Children.Add(transform);
+        }
+    }
+}
